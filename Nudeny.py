1def4d9db5250cd012cd269b8e3dd98219952082
marvinraydalida <marvinraydalida@gmail.com>
2022-11-30 01:53:16 +0800

    Separated request methods for path and url

--- Nudeny.py
+++ Nudeny.py
@@ -4,6 +4,7 @@ from PIL import Image
 from urllib.request import urlopen
 from urllib.parse import urlparse
 import validators
+import uuid
 
 
 class Detect:
@@ -11,7 +12,7 @@ class Detect:
     def __init__(self):
         self.URL = "http://127.0.0.1:8000/"
 
-    def save_to_path(self, prediction, save_path, detection_type="bbox"):
+    def save_to_path(self, prediction, save_path):
         url = prediction['url']
         url_path = urlparse(url).path
         filename = url_path.split('/')[2]
@@ -23,42 +24,30 @@ class Detect:
         else:
             raise Exception("Save path does not exist.")
 
-    def detectExposed(self, paths=[], urls=[], save_path=None):
+    def detectExposed(self, paths=[], save_path=None):
         files = []
         for path in paths:
             if not os.path.exists(path):
                 raise Exception("Path provided does not exists.")
             files.append(('files', open(path, 'rb')))
-        
-        for url in urls:
-            if not validators.url(url):
-                raise Exception("URL provided is not valid.")
-            image = urlopen(url).read()
-            files.append(('files', image))
 
         response = requests.post(
             url=self.URL+"draw_bounding_box/", files=files).json()
 
         predictions = response['predictions']
-        
+
         if not save_path == None:
             for prediction in predictions:
-                self.save_to_path(prediction, save_path, detection_type="bbox")
+                self.save_to_path(prediction, save_path)
 
         return response
 
-    def censorExposed(self, paths=[], urls=[], save_path=None):
+    def censorExposed(self, paths=[], save_path=None):
         files = []
         for path in paths:
             if not os.path.exists(path):
                 raise Exception("Path provided does not exists.")
             files.append(('files', open(path, 'rb')))
-        
-        for url in urls:
-            if not validators.url(url):
-                raise Exception("URL provided is not valid.")
-            image = urlopen(url).read()
-            files.append(('files', image))
 
         response = requests.post(
             url=self.URL+"censor/", files=files).json()
@@ -67,7 +56,54 @@ class Detect:
 
         if not save_path == None:
             for prediction in predictions:
-                self.save_to_path(prediction, save_path, detection_type="censor")
+                self.save_to_path(prediction, save_path)
 
         return response
 
+    def detectExposedFromUrl(self, urls=[], save_path=None):
+        json = []
+
+        for url in urls:
+            if not validators.url(url):
+                raise Exception("URL provided is not valid.")
+            image = urlopen(url)
+            extension = str(image.headers.get_content_type()).split('/')[1]
+            json.append({
+                "filename": str(uuid.uuid4()) + "." + extension,
+                "url": url
+            })
+
+        response = requests.post(
+            url=self.URL+"draw_bounding_boxUrl/", json=json).json()
+
+        predictions = response['predictions']
+
+        if not save_path == None:
+            for prediction in predictions:
+                self.save_to_path(prediction, save_path)
+
+        return response
+
+    def censorExposedFromUrl(self, urls=[], save_path=None):
+        json = []
+
+        for url in urls:
+            if not validators.url(url):
+                raise Exception("URL provided is not valid.")
+            image = urlopen(url)
+            extension = str(image.headers.get_content_type()).split('/')[1]
+            json.append({
+                "filename": str(uuid.uuid4()) + "." + extension,
+                "url": url
+            })
+
+        response = requests.post(
+            url=self.URL+"censorUrl/", json=json).json()
+
+        predictions = response['predictions']
+
+        if not save_path == None:
+            for prediction in predictions:
+                self.save_to_path(prediction, save_path)
+
+        return response
